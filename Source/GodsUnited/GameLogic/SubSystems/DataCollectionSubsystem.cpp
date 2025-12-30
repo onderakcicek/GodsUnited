9006e417c253a0521284a5f7b29fc4a4f7c7ecc1
Orcun As <contact@orcunas.com>
2025-12-31 00:38:37 +0300

    Update DataCollectionSubsystem.cpp

--- Source/GodsUnited/GameLogic/SubSystems/DataCollectionSubsystem.cpp
+++ Source/GodsUnited/GameLogic/SubSystems/DataCollectionSubsystem.cpp
@@ -21,30 +21,54 @@ void UDataCollectionSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 	if (!IsEditorRuntime() && bEnabled)
 	{
 		UGameAnalytics* GameAnalytics = FGameAnalyticsModule::Get().GetInstance();
+		
+		// Sets the userId, must be configured before initialization
+		// By default the GameAnalytics SDK will generate a default userId
+		// GameAnalytics->ConfigureUserId("myCustomUserId");
+
+		// setting the credentials
 		GameAnalytics->Initialize("3e8b6f26bcd6d4d3df004d6656838cbe", "d94836251566d6b6313e6f315fae16e4abe6c834");
+
+		// sets build version
+		// GameAnalytics->ConfigureBuild(TEXT("1.0.0"));
+		
+		// sets build version according to Project Settings -> Project Version
+		GameAnalytics->ConfigureAutoDetectAppVersion(true);
+
+		// Sets a secondary identifier that you can use to aggregate and link data sets
+		//GameAnalytics->ConfigureExternalUserId("iyo");
+
+		// If the app has the required permissions, GameAnalytics will track the advertising IDs on certain platforms such as Android and iOS.
+		// If you wish to disable tracking, for privacy reasons, you can call the following function
+		//GameAnalytics->EnableAdvertisingId(false);
+		
 		SessionStarted();
 	}
 }
 
 void UDataCollectionSubsystem::Deinitialize()
 {
-	if (!IsEditorRuntime() && bEnabled)
-	{
-		UGameAnalytics* GameAnalytics = FGameAnalyticsModule::Get().GetInstance();
-		GameAnalytics->EndSession();
-	}
+	SessionEnded();
 
 	Super::Deinitialize();
 }
 
 void UDataCollectionSubsystem::SessionStarted()
 {
-	UGameAnalytics* GameAnalytics = FGameAnalyticsModule::Get().GetInstance();
-	GameAnalytics->StartSession();
+	if (!IsEditorRuntime() && bEnabled)
+	{
+		UGameAnalytics* GameAnalytics = FGameAnalyticsModule::Get().GetInstance();
+		GameAnalytics->StartSession();	
+	}
 }
 
 void UDataCollectionSubsystem::SessionEnded()
 {
+	if (!IsEditorRuntime() && bEnabled)
+	{
+		UGameAnalytics* GameAnalytics = FGameAnalyticsModule::Get().GetInstance();
+		GameAnalytics->EndSession();
+	}
 }
 
 void UDataCollectionSubsystem::MatchStarted()
